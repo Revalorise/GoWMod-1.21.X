c8f503381145739ed510fde5f2544a3f9e836b81
Bon <Blackball007b@gmail.com>
2025-04-22 13:07:53 +0700

    added tooltips for custom item

--- src/main/java/net/revalorise/gowmod/item/ModItems.java
+++ src/main/java/net/revalorise/gowmod/item/ModItems.java
@@ -1,6 +1,9 @@
 package net.revalorise.gowmod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -8,6 +11,8 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import net.revalorise.gowmod.GodOfWarMod;
 import net.revalorise.gowmod.item.custom.ChiselItem;
 import net.revalorise.gowmod.item.custom.FuelItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(GodOfWarMod.MOD_ID);
@@ -22,7 +27,13 @@ public class ModItems {
             () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.gowmod.radish.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
             () -> new FuelItem(new Item.Properties(), 800));
