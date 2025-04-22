d4ced2a6ec4e2c9c430f6520fc89855d9700aec8
Bon <Blackball007b@gmail.com>
2025-04-22 15:15:41 +0700

    added data generator

---
+++ src/main/java/net/revalorise/gowmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,27 @@
+package net.revalorise.gowmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.revalorise.gowmod.GodOfWarMod;
+import net.revalorise.gowmod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, GodOfWarMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BISMUTH_BLOCK);
+        blockWithItem(ModBlocks.BISMUTH_ORE);
+        blockWithItem(ModBlocks.BISMUTH_DEEPSLATE_ORE);
+
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+    }
+
+    private void blockWithItem(DeferredBlock<?> deferredBlock) {
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
+}
